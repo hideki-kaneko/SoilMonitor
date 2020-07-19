667cb082adc6d26fbcf6ef0c08ef8b10ed83bc36
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-07-19 18:40:05 +0900

    initial commit

---
+++ SoilMonitor.ino
@@ -0,0 +1,12 @@
+#include <M5StickC.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.print("Hello M5!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
