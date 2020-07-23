156ff6a18432bef01b0d3a85710a9625caf4cbc2
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-07-23 11:18:37 +0900

    画面表示を調整

--- SoilMonitor.cpp
+++ SoilMonitor.cpp
@@ -9,6 +9,9 @@
 namespace
 {
     const int cBarDivNum = 10;
+    const int cBarFillMargin = 2;
+    const int cBarLevelLow = 3;
+    const int cBarLevelMid = 6;
     const int cCenterTextOffsetY = -6;
 }
 
@@ -16,13 +19,13 @@ namespace
 void SoilMonitor::setup()
 {
     M5.begin();
-    M5.Lcd.setRotation(0);
+    M5.Lcd.setRotation(2);
     drawBar_(0.6f);
 }
 //--------------------------------------------------------------------------------
 void SoilMonitor::loop()
 {
-    // M5.Lcd.fillScreen(TFT_BLACK);
+    M5.Lcd.fillScreen(TFT_BLACK);
     drawBar_(mCounter / 10.f);
     delay(1000);
     mCounter = (mCounter + 1) % 10;
@@ -39,11 +42,34 @@ void SoilMonitor::drawBar_(float ratio)
     for (int i = 0; i < cBarDivNum; i++)
     {
         const auto color = M5.lcd.color565(255, 255, 255);
-        M5.Lcd.drawRect(0, height - lectHeight * (i + 1), width, lectHeight, color);
+        M5.Lcd.drawRect(
+            0,
+            height - lectHeight * (i + 1),
+            width,
+            lectHeight,
+            color
+        );
         if (i < barNum)
         {
-            const auto color2 = M5.Lcd.color565(0, 255, 0);
-            M5.Lcd.drawRect(0, height - lectHeight * (i + 1), width, lectHeight, color2);
+            uint16_t color2;
+            if(i < cBarLevelLow){
+                color2 = M5.Lcd.color565(255, 0, 0);
+            }
+            else if (i < cBarLevelMid)
+            {
+                color2 = M5.Lcd.color565(255, 255, 0);
+            }
+            else
+            {
+                color2 = M5.Lcd.color565(0, 255, 0);
+            }
+            M5.Lcd.fillRect(
+                cBarFillMargin,
+                height - lectHeight * (i + 1) + cBarFillMargin,
+                width - cBarFillMargin * 2,
+                lectHeight - cBarFillMargin * 2,
+                color2
+            );
         }
     }
 
